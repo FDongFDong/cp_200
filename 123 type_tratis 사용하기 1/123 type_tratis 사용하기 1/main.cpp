33392f688fe6d6df9d1444eb1ad5c821209bf692
FDongFDong <abnormal5626@outlook.kr>
2018-10-02 22:32:28 +0900

    123 type_tratis 사용하기 1
    정수,enum,signed

---
+++ 123 type_tratis 사용하기 1/123 type_tratis 사용하기 1/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+enum TmpEnum {};
+enum class TmpEnumClass : int {};
+
+int main()
+{
+	cout << boolalpha;
+	cout << "== is_integral ==" << endl;
+	cout << is_integral<TmpEnum>::value << endl;
+	cout << is_integral<TmpEnumClass>::value << endl;
+	cout << is_integral<signed int>::value << endl;
+	cout << is_integral<unsigned int>::value << endl;
+	cout << is_integral<double>::value << endl;
+	cout << is_integral<bool>::value << endl;
+
+	cout << "== is_enum ==" << endl;
+	cout << is_enum<TmpEnum>::value << endl;
+	cout << is_enum<TmpEnumClass>::value << endl;
+	cout << is_enum<int>::value << endl;
+
+	cout << "== is_signed ==" << endl;
+	cout << is_signed<TmpEnum>::value << endl;
+	cout << is_signed<signed int>::value << endl;
+	cout << is_signed<unsigned int>::value << endl;
+
+	cout << "== is_unsigned ==" << endl;
+	cout << is_unsigned<TmpEnumClass>::value << endl;
+	cout << is_unsigned<int>::value << endl;
+	cout << is_unsigned<unsigned int>::value << endl;
+	system("pause");
+	return 0;
+}
+//is_integral은 정수 계열의 형식인지 검사
+//is_enum은 enum계열의 형식인지 검사
+//is_unsigned는 unsigned 정수 계열의 형식인지 검사
