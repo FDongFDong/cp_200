48f3f78667dab869449cd92db1a00253393f5f37
FDongFDong <abnormal5626@outlook.kr>
2018-10-02 19:32:43 +0900

    121 함수 오버라이딩 이해하기

---
+++ 121 함수 오버라이딩 이해하기/121 함수 오버라이딩 이해하기/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class Base
+{
+public:
+	void Display()
+	{
+		cout << "고려 16대왕 예종" << endl;
+	}
+};
+class Derived : Base
+{
+public:
+	void Display()
+	{
+		cout << "고려 17대왕 인종" << endl;
+	}
+};
+int main()
+{
+	Derived child;
+	child.Display();
+	system("pause");
+
+	return 0;
+}
